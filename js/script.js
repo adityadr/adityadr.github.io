60f6b6e321451076d9946834747fd10237efd4f7
Aditya <draditya1991@gmail.com>
2015-05-27 20:29:52 +0530

    Adding a profile page here

---
+++ js/script.js
@@ -0,0 +1,45 @@
+
+//========================
+// GA
+//========================
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-63432268-1', 'auto');
+  ga('send', 'pageview');
+  
+//========================
+//CUSTOM SCROLLBAR
+//========================
+$(document).ready(
+    function () {
+        $("html").niceScroll({
+            mousescrollstep: 70,
+            cursorcolor: "#2ca8f5",
+            cursorwidth: "6px",
+            cursorborderradius: "5px",
+            cursorborder: "none",
+        });
+    }
+);
+
+
+//========================
+//SMOOTHSCROLL
+//========================
+$(function () {
+    $('a[href*=#]:not([href=#])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html,body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
